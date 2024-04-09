83f6fd3423409de3b4b0d0ba385cdefa31e75a95
Denis Kalkopf <deniskalkopf@gmail.com>
2024-04-09 16:58:38 +0500

    fix

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -14,59 +14,61 @@ export default function Table() {
 
   return (
     <table className={styles.table}>
-      <tr className={styles.row}>
-        <th className={styles.head}>Показатель</th>
-        <th className={`${styles.head} ${styles.bgBlue}`}>Текущий день</th>
-        <th className={styles.head}>Вчера</th>
-        <th className={styles.head}>Этот день недели</th>
-      </tr>
-      {data.length &&
-        data.map((el) => (
-          <>
-            <tr key={el.id} onClick={() => setActiveGraph(el.id)}>
-              <td className={styles.cell}>{el.title} руб.</td>
-              <td className={`${styles.cell} ${styles.bgBlue}`}>{el.thisDay}</td>
-              <td
-                className={cn(styles.cell, {
-                  [styles.bgRed]: percents(el.thisDay, el.yesterday) < 0,
-                  [styles.bgGreen]: percents(el.thisDay, el.yesterday) > 0,
-                })}
-              >
-                {el.yesterday}
-                <span
-                  className={cn(styles.percents, styles.colorGreen, {
-                    [styles.colorRed]: percents(el.thisDay, el.yesterday) < 0,
+      <tbody>
+        <tr className={styles.row}>
+          <th className={styles.head}>Показатель</th>
+          <th className={`${styles.head} ${styles.bgBlue}`}>Текущий день</th>
+          <th className={styles.head}>Вчера</th>
+          <th className={styles.head}>Этот день недели</th>
+        </tr>
+        {data.length &&
+          data.map((el) => (
+            <>
+              <tr key={el.id} onClick={() => setActiveGraph(el.id)}>
+                <td className={styles.cell}>{el.title} руб.</td>
+                <td className={`${styles.cell} ${styles.bgBlue}`}>{el.thisDay}</td>
+                <td
+                  className={cn(styles.cell, {
+                    [styles.bgRed]: percents(el.thisDay, el.yesterday) < 0,
+                    [styles.bgGreen]: percents(el.thisDay, el.yesterday) > 0,
                   })}
                 >
-                  {percents(el.thisDay, el.yesterday)}%
-                </span>
-              </td>
-              <td
-                className={cn(styles.cell, {
-                  [styles.bgRed]: el.thisDayWeek > el.thisDay,
-                  [styles.bgGreen]: el.thisDayWeek < el.thisDay,
-                })}
-              >
-                {el.thisDayWeek}
-              </td>
-            </tr>
-            {el.id === activeGraph ? (
-              <tr>
-                <td colSpan={4}>
-                  <Graph
-                    id={el.id}
-                    title={el.title}
-                    thisDay={el.thisDay}
-                    yesterday={el.yesterday}
-                    thisDayWeek={el.thisDayWeek}
-                  />
+                  {el.yesterday}
+                  <span
+                    className={cn(styles.percents, styles.colorGreen, {
+                      [styles.colorRed]: percents(el.thisDay, el.yesterday) < 0,
+                    })}
+                  >
+                    {percents(el.thisDay, el.yesterday)}%
+                  </span>
+                </td>
+                <td
+                  className={cn(styles.cell, {
+                    [styles.bgRed]: el.thisDayWeek > el.thisDay,
+                    [styles.bgGreen]: el.thisDayWeek < el.thisDay,
+                  })}
+                >
+                  {el.thisDayWeek}
                 </td>
               </tr>
-            ) : (
-              ''
-            )}
-          </>
-        ))}
+              {el.id === activeGraph ? (
+                <tr>
+                  <td colSpan={4}>
+                    <Graph
+                      id={el.id}
+                      title={el.title}
+                      thisDay={el.thisDay}
+                      yesterday={el.yesterday}
+                      thisDayWeek={el.thisDayWeek}
+                    />
+                  </td>
+                </tr>
+              ) : (
+                ''
+              )}
+            </>
+          ))}
+      </tbody>
     </table>
   );
 }
